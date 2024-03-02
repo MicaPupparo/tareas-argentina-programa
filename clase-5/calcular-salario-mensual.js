b662c03cbf4fb186b7628ba09ee9c2618708542f
MicaPupparo <micaelapupparo@gmail.com>
2024-03-02 19:52:39 -0300

    error corregido en las variables html
    había puesto $ al inicio de las variable que les asignaba el valor (.value) de un elemento de html

--- clase-5/calcular-salario-mensual.js
+++ clase-5/calcular-salario-mensual.js
@@ -1,8 +1,8 @@
 const $botonCalcularSalarioMensual = document.querySelector("#calcular-salario-mensual");
 
 $botonCalcularSalarioMensual.onclick = function(){
-    const $salarioAnualUsuario = Number(document.querySelector("#salario-anual").value);
-    let salarioMensual = calcularSalarioMensual($salarioAnualUsuario);
+    const salarioAnualUsuario = Number(document.querySelector("#salario-anual").value);
+    let salarioMensual = calcularSalarioMensual(salarioAnualUsuario);
 
     document.querySelector("#salario-mensual").value = salarioMensual;
     return false;
