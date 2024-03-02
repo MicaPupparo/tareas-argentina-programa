719da69718808adfdcee72c97f8e990af7e26e5e
MicaPupparo <micaelapupparo@gmail.com>
2024-03-02 19:39:08 -0300

    tarea bienvenido usuario
    segunda parte de la tarea a realizar

---
+++ clase-5/bienvenido-usuario.js
@@ -0,0 +1,16 @@
+const $botonEnviarDatosUsuario = document.querySelector("#enviar-datos-usuario");
+
+$botonEnviarDatosUsuario.onclick = function(){
+    const $primerNombreUsuario = document.querySelector("#primer-nombre-usuario").value;
+    const $segundoNombreUsuario = document.querySelector("#segundo-nombre-usuario").value;
+    const $edadUsuario = Number(document.querySelector("#edad-usuario").value);
+
+    document.querySelector("h1").innerText = `Bienvenido, ${$primerNombreUsuario}!`;
+    document.querySelector("#primer-nombre-resultado").innerText = `Tu primer nombre es ${$primerNombreUsuario}`;
+    document.querySelector("#segundo-nombre-resultado").innerText = `Tu segundo nombre es ${$segundoNombreUsuario}`;
+    document.querySelector("#edad-resultado").innerText = `Tu edad es ${$edadUsuario}`;
+
+    return false;
+}
+
+
