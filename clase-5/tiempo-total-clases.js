b7b38d4da72ae768bb6d0809a21759cdf7d276b0
MicaPupparo <micaelapupparo@gmail.com>
2024-03-03 00:05:15 -0300

    agregue tarea de calcular el tiempo total de los videos

---
+++ clase-5/tiempo-total-clases.js
@@ -0,0 +1,28 @@
+let horasTotales = 0;
+let minutosTotales = 0;
+let segundosTotales = 0;
+
+const $botonEnviarDatosTiempo = document.querySelector("#enviar-tiempo");
+
+$botonEnviarDatosTiempo.onclick = function() {
+    const horasEnElVideo = Number(document.querySelector("#horas-video").value);
+    const minutosEnElVideo = Number(document.querySelector("#minutos-video").value);
+    const segundosEnElVideo = Number(document.querySelector("#segundos-video").value);
+
+    horasTotales += horasEnElVideo;
+
+    minutosTotales += minutosEnElVideo;
+    if (minutosTotales > 59) {
+        minutosTotales = minutosTotales - 60;
+        horasTotales++;
+    }
+    
+    segundosTotales += segundosEnElVideo;
+    if (segundosTotales > 59) {
+        segundosTotales = segundosTotales - 60;
+        minutosTotales++;
+    }
+
+    document.querySelector("#tiempo-total").innerText = `El tiempo total es de ${horasTotales} horas, ${minutosTotales} minutos y ${segundosTotales} segundos.`;
+    return false;
+}
